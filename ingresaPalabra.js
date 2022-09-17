78110ebc520a8b27da90b64fb9633d61e94e41d1
Tessera04 <matute_11@hotmail.com.ar>
2022-09-17 19:36:49 -0300

    Retoques a la estructura y actualizacion de funciones

---
+++ ingresaPalabra.js
@@ -0,0 +1,14 @@
+/* Botones selectores */
+
+const playBtn = document.querySelector('.jugarInicio');
+const addWordBtn = document.querySelector(".addWordInicio");
+const saveBtn = document.querySelector(".saveWord");
+const inicioBtn = document.querySelector(".goInicio");
+const cancelBtn = document.querySelector(".cancelGame");
+const inicio2Btn = document.querySelector(".inicioGame");
+
+/* Selectores de pantalla */
+
+const mainScreen = document.querySelector(".inicio");
+const addScreen = document.querySelector(".addWord");
+const gameScreen = document.querySelector(".gameScreen");
